8c2932fce45d3e0ab725292d226a6d1db6d6eb08
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-02-16 15:53:02 +0100

    working on home-section-1

--- src/components/Home/Body/Body.jsx
+++ src/components/Home/Body/Body.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-
+import { Container, Button, Row, Col } from "react-bootstrap";
 const Body = () => {
-    return(
+    return (
         <div>
-            
+            <section style={{  'marginTop': '50px' }} >
+
+                <Container>
+                    <Row>
+                        <Col>
+                            <div >
+
+                            <div >
+                                <h1>raising the next african landlords</h1>
+                            </div>
+
+                            <div>
+                                <p>we provide clients with opportunities to own valuable
+                                    commercial and residential land and housing properties
+                                    in stategic areas in nigeria.</p>
+                            </div>
+
+                            <div>
+                                <Button variant="dark">explore</Button>
+                            </div>
+
+                            </div>
+                        </Col>
+
+                        <Col><div >
+                            <img src="/images/section1.png" alt="logo image" />
+                        </div></Col>
+                    </Row>
+
+                </Container>
+
+
+            </section>
+
         </div>
     )
 }
