54a3e76d467506f6f8dd7a5796b029754b4a090b
abimbolataofeek <65461446+abimbolataofeek@users.noreply.github.com>
2022-02-27 17:06:03 +0100

    footer css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Footer.css";
 import { Container, Button, Row, Col } from "react-bootstrap";
-import { MdLocationOn,MdEmail, MdCall} from 'react-icons/md';
-import {BsFacebook } from 'react-icons/bs';
-import {VscTwitter } from 'react-icons/vsc';
-import {FaLinkedinIn } from 'react-icons/fa';
+import { MdLocationOn, MdEmail, MdCall } from 'react-icons/md';
+import { BsFacebook } from 'react-icons/bs';
+import { VscTwitter } from 'react-icons/vsc';
+import { FaLinkedinIn } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer-container">
             <section>
                 <Container>
                     <Row md={4}>
@@ -17,28 +17,28 @@ const Footer = () => {
                         </Col>
                         <Col>
                             <h6 className="footer-heading">Our office</h6>
-                            <p> <MdLocationOn /> nigeria</p>
+                            <a href="#" className="a"> <p><MdLocationOn  /> nigeria</p></a>
                             <p>No. 4 Oringwo street, Sani Abacha
                                 road, GRA phase III, Port Harcourt.</p>
-                            <p><MdLocationOn />kenya</p>
+                            <a href="#" className="a"> <p><MdLocationOn /> kenya</p></a>
                             <p>Suite M02, Madonna House Westland, Nairobi. P.O.Box 14196- 00800.</p>
                         </Col>
                         <Col xs={6}>
                             <h6 className="footer-heading">Start a conversation</h6>
-                            <p><MdEmail/>email</p>
-                            <p>support@bamboogroupng.com</p>
-                            <p><MdCall/>phone</p>
-                            <p>+234 905 407 8990</p>
-                            <p>+234 703 139 6510</p>
+                            
+                            <a href="#" className="a"> <p><MdEmail /> email</p></a>
+                            <a href="#" className="a"><p>support@bamboogroupng.com</p></a>
+                            <a href="#" className="a"> <p><MdCall /> phone</p></a>                            
+                            <a href="#" className="a"><p>+234 905 407 8990</p></a> 
+                            <a href="#" className="a"> <p>+234 703 139 6510</p></a>
                         </Col>
 
                         <Col>
                             <h6 className="footer-heading">Follow Us</h6>
-                            <p> <BsFacebook className="icons-color" /> facebook</p>
-                            <p><VscTwitter/>twitter</p>
-                            <p><FaLinkedinIn/>Linkedin</p>
-                            <p>+234 905 407 8990</p>
-                            <p>+234 703 139 6510</p>
+                            <a href="#" className="a"> <p><BsFacebook /> facebook</p></a>
+                            <a href="#" className="a"> <p><VscTwitter /> twitter</p></a>
+                            <a href="#" className="a"> <p><FaLinkedinIn /> Linkedin</p></a>
+                            
                         </Col>
                     </Row>
                 </Container>
